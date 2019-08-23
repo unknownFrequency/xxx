9b102180d2c253d7739323e52ade76a62ac2d6a9
Your Name <unknownfreq@protonmail.com>
2019-08-23 07:47:14 +0200

    orphaned commit

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,4 +1,6 @@
 class Gallery < ApplicationRecord
   has_many :gallery_attachments
   accepts_nested_attributes_for :gallery_attachments
+
+  validates :title, presence: true
 end
--- app/models/gallery_attachment.rb
+++ app/models/gallery_attachment.rb
@@ -1,6 +1,8 @@
 class GalleryAttachment < ApplicationRecord
   belongs_to :user
   belongs_to :gallery
-
   mount_uploader :picture, PicturesUploader
+
+  validates :user_id, presence: true
+  validates :picture, presence: true
 end
